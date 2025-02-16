57329e10d8f7f6b1a2d13093076613a7782001e2
chrpkorir <faythkoko@gmail.com>
2025-02-16 14:16:01 +0300

    Addded fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        return"Invalid"
+    elif length == 1:
+        return [0]
+    elif length == 2:
+        return [0, 1]
+    
+    series = [0, 1]
+    while len(series) < length:
+        series.append(series[-1] + series[-2])
+
+    return series
+
+print(print_fibonacci(10))
